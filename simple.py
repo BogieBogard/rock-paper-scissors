f2d449c1501a515d893e33095dabde040b2e079e
BogieBogard <mbogard@utexas.edu>
2023-02-12 22:31:25 -0600

    Python page connection with GCP container backend

--- simple.py
+++ simple.py
@@ -1,40 +1,25 @@
 from fastapi import FastAPI
-import math
+from starlette.responses import JSONResponse
 
 app = FastAPI()
-import random
 
-@app.post("/game")
+@app.get("/game")
 async def game(player1_choice: str, player1_name_input: str, player2_name_input: str):
-    round_number = 0
-    player1_score = 0
-    player2_score = 0
-    computer_score = 0
-
-    choices = ["rock", "paper", "scissors"]
-    computer_choice = random.choice(choices)
-
+    import random
+    round_number = 1
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
     if player1_choice == computer_choice:
-        winner = "It's a tie! Both chose " + player1_choice
-    elif player1_choice == "rock" and computer_choice == "scissors":
-        winner = player1_name_input + " wins this round."
-        player1_score += 1
-    elif player1_choice == "rock" and computer_choice == "paper":
-        winner = "The computer wins this round."
-        computer_score += 1
-    elif player1_choice == "paper" and computer_choice == "rock":
-        winner = player1_name_input + " wins this round."
-        player1_score += 1
-    elif player1_choice == "paper" and computer_choice == "scissors":
-        winner = "The computer wins this round."
-        computer_score += 1
-    elif player1_choice == "scissors" and computer_choice == "paper":
-        winner = player1_name_input + " wins this round."
-        player1_score += 1
-    elif player1_choice == "scissors" and computer_choice == "rock":
-        winner = "The computer wins this round."
-        computer_score += 1
-
-    round_number += 1
-
-    return {"winner": winner, "round_number": round_number, "player1_score": player1_score, "computer_score": computer_score}
+        winner = "It's a tie! Both chose {}".format(player1_choice)
+        player1_score = 0
+        computer_score = 0
+    elif (player1_choice == "rock" and computer_choice == "scissors") or (player1_choice == "paper" and computer_choice == "rock") or (player1_choice == "scissors" and computer_choice == "paper"):
+        winner = "{} wins! {} chose {}, {} chose {}".format(player1_name_input, player1_name_input, player1_choice, player2_name_input, computer_choice)
+        player1_score = 1
+        computer_score = 0
+    else:
+        winner = "{} wins! {} chose {}, {} chose {}".format(player2_name_input, player1_name_input, player1_choice, player2_name_input, computer_choice)
+        player1_score = 0
+        computer_score = 1
+    response = {"winner": winner, "round_number": round_number, "player1_score": player1_score, "computer_score": computer_score}
+    return JSONResponse(content=response, headers={"Access-Control-Allow-Origin": "*"})
